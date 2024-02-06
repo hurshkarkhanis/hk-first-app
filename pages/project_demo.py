623e709fec645904280e04dedf9c15aed6ff1380
Hursh Karkhanis <karkhanis.hursh@gmail.com>
2024-02-06 01:32:36 +0000

    end of day 2/5

--- pages/project_demo.py
+++ pages/project_demo.py
@@ -1,11 +1,16 @@
 import yfinance as yf
 import streamlit as st
 import pandas as pd
+import time
+import numpy as np
+
 
 
 
 
 msft = yf.Ticker("MSFT")
+
+
 
 
 # get all stock info
@@ -14,4 +19,21 @@ month_price = msft.history(period="1mo")
 
 
 st.dataframe(month_price)
+st.title("hk")
 
+activated = st.toggle("Background App Refresh")
+
+
+st.write("hk")
+st.divider()
+st.write("sk")
+
+oscar = pd.read_csv("/workspaces/hk-first-app/pages/oscar.csv")
+
+st.bar_chart(oscar.set_index("Year")["Age"])
+
+st.data_editor(oscar)
+
+st.metric("points", 30, 2)
+
+msft.insider_transactions
